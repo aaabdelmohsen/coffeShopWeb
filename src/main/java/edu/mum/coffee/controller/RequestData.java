2dc2030de0277af4ca789b2acb831cc92e3c9eb8
aaabdelmohsen <40072988+aaabdelmohsen@users.noreply.github.com>
2018-06-10 17:53:40 -0500

    rest Client

---
+++ src/main/java/edu/mum/coffee/controller/RequestData.java
@@ -0,0 +1,29 @@
+package edu.mum.coffee.controller;
+
+public class RequestData {
+
+	private String url;
+	private String body;
+	private String response;
+	
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+}
---
+++ src/main/java/edu/mum/coffee/controller/RestClientController.java
@@ -0,0 +1,30 @@
+package edu.mum.coffee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class RestClientController {
+
+	
+	@GetMapping("/restClient")
+	public String restClient() {	
+		return "rest";
+	}
+	
+	@PostMapping("/processResquest")
+	public String processResquest(@ModelAttribute("requestData") RequestData requestData, BindingResult result) {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Response response = restTemplate.postForObject(requestData.getUrl(), requestData.getBody(), Response.class);
+		System.out.println("response : " + response);
+		
+		return "rest";
+	}
+	
+	
+}
